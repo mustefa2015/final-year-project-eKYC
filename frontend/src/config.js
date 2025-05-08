2fcc678219ef9f07d9f32079751e9d9a201e19f1
mustefa2015 <133257659+mustefa2015@users.noreply.github.com>
2025-05-08 20:24:52 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 //export const BASE_URL = "https://wst-webapp-production.up.railway.app/api/v1";
-export const BASE_URL = "https://myfayda-backend-portal.up.railway.app:5000/api/v1";
+export const BASE_URL = "https://myfayda-backend-portal.up.railway.app/api/v1";
 export const token = localStorage.getItem("token");
