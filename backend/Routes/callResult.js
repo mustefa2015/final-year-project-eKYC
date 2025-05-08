8a61c62398a65ef170a4b7bd7d1cf2aad143d03b
mustefa2015 <133257659+mustefa2015@users.noreply.github.com>
2025-05-08 19:57:17 +0300

    Delete backend directory

--- backend/Routes/callResult.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUserData } from '../Controllers/callResultController.js';
-
-const router = express.Router();
-
-// POST endpoint for fetching user data
-router.post('/', getUserData);
-
-export default router;
